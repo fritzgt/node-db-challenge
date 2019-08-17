d649afefc0099ec0090f9f3fe9fdc115a783d9f8
fritzgt <tuanis@live.com>
2019-08-17 06:34:04 +0300

    adding seeds

--- model/projects-model.js
+++ model/projects-model.js
@@ -27,10 +27,19 @@ function remove(id) {}
 //Task
 
 function findTaskById(id) {
-  return db
-    .select('*')
-    .from('task')
-    .where('project_id', id);
+  return (
+    db('task as t')
+      .join('project as p', 't.project_id', 'p.id')
+      .select(
+        'p.name as project_name',
+        'p.description as project_description',
+        't.description as task_description',
+        't.notes as task_notes',
+        't.completed as task_completed'
+      )
+      // .from('task')
+      .where('project_id', id)
+  );
 }
 
 function addTask(newTask) {
