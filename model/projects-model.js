0b9f0158b938417f9760bdae53e8f1c091c0f9c5
fritzgt <tuanis@live.com>
2019-08-17 07:09:57 +0300

    Completed MVP

--- model/projects-model.js
+++ model/projects-model.js
@@ -27,19 +27,16 @@ function remove(id) {}
 //Task
 
 function findTaskById(id) {
-  return (
-    db('task as t')
-      .join('project as p', 't.project_id', 'p.id')
-      .select(
-        'p.name as project_name',
-        'p.description as project_description',
-        't.description as task_description',
-        't.notes as task_notes',
-        't.completed as task_completed'
-      )
-      // .from('task')
-      .where('project_id', id)
-  );
+  return db('task as t')
+    .join('project as p', 't.project_id', 'p.id')
+    .select(
+      'p.name as project_name',
+      'p.description as project_description',
+      't.description as task_description',
+      't.notes as task_notes',
+      't.completed as task_completed'
+    )
+    .where('project_id', id);
 }
 
 function addTask(newTask) {
